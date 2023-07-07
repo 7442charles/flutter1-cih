1ac4e6f0895b450d247edc312e3aa002569d2538
7442charles <cking6951@gmail.com>
2023-07-07 07:27:33 +0300

    second main branch cleanup

--- lib/Features/students_portal.dart
+++ lib/Features/students_portal.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-class StudentsPortalApp extends StatelessWidget {
-  const StudentsPortalApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Student Portal',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: StudentsPortalPage(),
-    );
-  }
-}
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class StudentsPortalPage extends StatefulWidget {
   const StudentsPortalPage({Key? key}) : super(key: key);
@@ -27,7 +13,7 @@ class _StudentsPortalPageState extends State<StudentsPortalPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _admissionNumberController =
       TextEditingController();
-  String _searchResult = '';
+  List<Map<String, dynamic>> _searchResults = [];
   bool _isLoading = false;
 
   @override
@@ -37,25 +23,26 @@ class _StudentsPortalPageState extends State<StudentsPortalPage> {
     super.dispose();
   }
 
-  Future<Map<String, dynamic>?> fetchStudentResult(
+  Future<List<Map<String, dynamic>>?> fetchStudentResult(
       String name, String admissionNumber) async {
-    DatabaseReference databaseReference =
-        FirebaseDatabase.instance.reference().child('students');
+    const String url =
+        'https://raw.githubusercontent.com/7442charles/ecascade_jsons/main/results.json';
 
-    DataSnapshot snapshot = (await databaseReference.once()) as DataSnapshot;
+    final response = await http.get(Uri.parse(url));
 
-    if (snapshot.value != null) {
-      Map<dynamic, dynamic>? data = snapshot.value as Map<dynamic, dynamic>?;
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as Map<String, dynamic>;
+      final students = data['students'] as List<dynamic>;
 
-      if (data != null) {
-        for (final studentEntry in data.entries) {
-          Map<dynamic, dynamic> student = studentEntry.value;
+      List<Map<String, dynamic>> results = students
+          .where((student) =>
+              student['name'] == name &&
+              student['admissionNumber'] == admissionNumber)
+          .toList()
+          .cast<Map<String, dynamic>>();
 
-          if (student['name'] == name &&
-              student['admissionNumber'] == admissionNumber) {
-            return student['units'];
-          }
-        }
+      if (results.isNotEmpty) {
+        return results;
       }
     }
 
@@ -63,12 +50,27 @@ class _StudentsPortalPageState extends State<StudentsPortalPage> {
   }
 
   void _submitForm() async {
-    String name = _nameController.text;
-    String admissionNumber = _admissionNumberController.text;
+    String name = _nameController.text.trim();
+    String admissionNumber =
+        _admissionNumberController.text.trim().toUpperCase();
+
+    if (name.isEmpty || admissionNumber.isEmpty) {
+      _showErrorDialog('Please enter both name and admission number.');
+      return;
+    }
+
+    List<String> nameParts = name.split(' ');
+    nameParts = nameParts.map((part) {
+      if (part.isNotEmpty) {
+        return part[0].toUpperCase() + part.substring(1);
+      }
+      return '';
+    }).toList();
+    name = nameParts.join(' ');
 
     setState(() {
       _isLoading = true;
-      _searchResult = '';
+      _searchResults = [];
     });
 
     final resultData = await fetchStudentResult(name, admissionNumber);
@@ -79,16 +81,39 @@ class _StudentsPortalPageState extends State<StudentsPortalPage> {
 
     if (resultData != null) {
       setState(() {
-        _searchResult = 'Search result for $name - $admissionNumber\n\n';
-        resultData.forEach((unit, grade) {
-          _searchResult += '$unit: $grade\n';
-        });
+        _searchResults = resultData;
       });
     } else {
       setState(() {
-        _searchResult = 'No result found for $name - $admissionNumber';
+        _searchResults = [];
       });
+      _showErrorDialog(
+          'No result found for \n $name \n ${admissionNumber.toUpperCase()}');
     }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Please Try Again'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _downloadResults() {
+    // Write code here to download the results as a PDF file
   }
 
   @override
@@ -160,11 +185,66 @@ class _StudentsPortalPageState extends State<StudentsPortalPage> {
                     ),
             ),
             const SizedBox(height: 24.0),
-            Text(
-              _searchResult,
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+            Expanded(
+              child: ListView.builder(
+                itemCount: _searchResults.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final student = _searchResults[index];
+                  final studentName = student['name'];
+                  final admissionNumber = student['admissionNumber'];
+                  final units = student['units'] as Map<String, dynamic>;
+
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            studentName,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          subtitle: Text(
+                            admissionNumber,
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                        ExpansionTile(
+                          title: const Text(
+                            'View Results',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: const ClampingScrollPhysics(),
+                              itemCount: units.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                final unitName = units.keys.elementAt(index);
+                                final unitScore = units[unitName];
+                                return ListTile(
+                                  title: Text(unitName),
+                                  subtitle: Text('Score: $unitScore'),
+                                );
+                              },
+                            ),
+                            TextButton(
+                              onPressed: _downloadResults,
+                              child: const Text('Download Results'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ],
