381ab2c65800666808e394605c1995d6ab33d217
7442charles <cking6951@gmail.com>
2023-07-12 16:49:17 +0300

    notice board finally working

--- lib/Features/notice_board.dart
+++ lib/Features/notice_board.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'dart:convert';
 
 class Notice {
-  final String ref;
+  final String department;
   final String sender;
   final String to;
+  final String ref;
   final String message;
   final DateTime timestamp;
 
   Notice({
-    required this.ref,
+    required this.department,
     required this.sender,
     required this.to,
+    required this.ref,
     required this.message,
     required this.timestamp,
   });
@@ -35,27 +37,33 @@ class _NoticeBoardPageState extends State<NoticeBoardPage> {
   }
 
   void fetchNotices() async {
-    final response = await http.get(Uri.https(
-        '//raw.githubusercontent.com/7442charles/ecascade_jsons/main/noticeboard.json'));
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      List<Notice> fetchedNotices = [];
+    try {
+      final response = await Dio().get(
+          'https://raw.githubusercontent.com/7442charles/ecascade_jsons/main/noticeboard.json');
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.data);
 
-      jsonData.forEach((data) {
-        fetchedNotices.add(
-          Notice(
-            ref: data['ref'],
-            sender: data['sender'],
-            to: data['to'],
-            message: data['message'],
-            timestamp: DateTime.parse(data['timestamp']),
-          ),
-        );
-      });
+        List<Notice> fetchedNotices = [];
 
-      setState(() {
-        notices = fetchedNotices;
-      });
+        jsonData['noticeboard'].forEach((key, data) {
+          fetchedNotices.add(
+            Notice(
+              department: data['department'],
+              sender: data['sender'],
+              to: data['to'],
+              ref: data['ref'],
+              message: data['message'],
+              timestamp: DateTime.parse(data['timestamp']),
+            ),
+          );
+        });
+
+        setState(() {
+          notices = fetchedNotices;
+        });
+      }
+    } catch (error) {
+      print('Error: $error');
     }
   }
 
@@ -70,22 +78,86 @@ class _NoticeBoardPageState extends State<NoticeBoardPage> {
         itemBuilder: (context, index) {
           Notice notice = notices[index];
 
-          return ExpansionTile(
-            title: Text(notice.ref),
-            children: [
-              ListTile(
-                title: Text('Sender: ${notice.sender}'),
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 2.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text(
+                      notice.ref,
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      notice.department,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: <TextSpan>[
+                              const TextSpan(
+                                text: 'Sender: ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(text: notice.sender),
+                            ],
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: <TextSpan>[
+                              const TextSpan(
+                                text: 'To: ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(text: notice.to),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          notice.message,
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Posted: ${notice.timestamp.toString()}',
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              ListTile(
-                title: Text('To: ${notice.to}'),
-              ),
-              ListTile(
-                title: Text('Message: ${notice.message}'),
-              ),
-              ListTile(
-                title: Text('Posted: ${notice.timestamp.toString()}'),
-              ),
-            ],
+            ),
           );
         },
       ),
